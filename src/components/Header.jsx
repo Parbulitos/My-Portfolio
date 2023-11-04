c3d82b03a8a72641ec20bc903c9de14b099cb560
Parbulitos <cechavarne@gmail.com>
2023-11-04 22:51:44 +0100

    Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
     return(
         <div className="w-full px-6 py-2 flex items-center bg-emerald-500 border-solid bg-opacity-75 drop-shadow-darker">
             <div className="w-fit drop-shadow-dark rounded-full ">
-                <img src={ProfileImg} className="w-18 aspect-square border-2 drop-shadow-md rounded-full hover:cursor-pointer hover:scale-small transition-transform duration-medium"/>
+                <img src={ProfileImg} className="w-18 aspect-square border-2 drop-shadow-md rounded-full hover:cursor-pointer hover:scale-small transition-transform duration-medium" alt="Error loading resource"/>
             </div>
             <div className="h-14 p-2 mx-2 flex items-center font-barlow text-3xl rounded hover:cursor-pointer transition ease-linear hover:text-slate-300 hover:shadow-dark hover:scale-small  duration-short">
                 PROJECTS
